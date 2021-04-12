6f78dc10c3f974cee1dd82619546b295a7b26951
Zack <zackakil94@gmail.com>
2021-04-12 17:00:00 +0100

    binded data to edit form

--- web app/public/components/score-editor.js
+++ web app/public/components/score-editor.js
@@ -32,34 +32,43 @@ Vue.component('score-editor', {
                 ...this.match_details.home_team_players,
                 { 'name': '---AWAY TEAM---', 'id': null },
                     ...this.match_details.away_team_players]
-            
         }
     },
     template: `
     <div class="score-editor">
 
-    player <select name="cars" id="cars">
-        <option v-for="player in all_players" value="player.id">{{player.name}}</option>
-    </select>
-    <br>
-    points <input type="number" name="points" value=1>
-    <br>
-    distance <input type="number" name="distance" min=0 value=1>
-    <br>
-    segment start <input type="number" name="segment_start" min=0 value=1>
-    <br>
-    exact time <input type="number" name="exact_time" min=0 value=1>
-    <br>
-    segment end <input type="number" name="segment_end" min=0 value=1>
-    <br>
+        <div v-if="event_to_edit">
+            player <select name="cars" id="cars" v-bind:value="event_to_edit.player.id">
+                <option v-for="player in all_players" v-bind:value="player.id">{{player.name}}</option>
+            </select>
+            <br>
+            points <input type="number" name="points" value=1
+            v-bind:value="event_to_edit.points">
+            <br>
+            distance <input type="number" name="distance" min=0 value=1
+            v-bind:value="event_to_edit.run_distance">
+            <br>
+            segment start <input type="number" name="segment_start" min=0 
+            v-bind:value="event_to_edit.video_seconds_start">
+            <br>
+            exact time <input type="number" name="exact_time" min=0 value=1
+            v-bind:value="event_to_edit.video_seconds_exact">
+            <br>
+            segment end <input type="number" name="segment_end" min=0 value=1
+            v-bind:value="event_to_edit.video_seconds_end">
+            <br>
 
-    home <input type="radio" name="team" value="home">
-    away <input type="radio" name="team" value="away">
-    <br>
-    try line position<input type="range" name="score_position" min=0 max=10 value=5>
-    <br>
+            home <input type="radio" name="team" value="home" 
+            v-bind:checked="event_to_edit.match_side == 'home'">
+            away <input type="radio" name="team" value="away"
+            v-bind:checked="event_to_edit.match_side == 'away'">
+            <br>
+            try line position<input type="range" name="score_position" min=0 max=10 value=5
+            v-bind:value="event_to_edit.score_position">
+            <br>
 
-    <button>Save</button>
+            <button>Save</button>
+        </div>
 
     </div>
     `
